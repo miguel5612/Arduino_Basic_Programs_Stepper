862aa3e287b704509f960157a65372e3bd3b4acf
miguel5612 <miguelangelcu@ufps.edu.co>
2020-06-23 12:36:24 -0500

    Fixed stepper

--- 02.GiroControlled/02.GiroControlled.ino
+++ 02.GiroControlled/02.GiroControlled.ino
@@ -14,13 +14,15 @@
 
 
 // All the wires needed for full functionality
-#define DIR 55
-#define STEP 54
-#define pot A3
-#define relay 13
+#define DIR 6
+#define STEP 3
+#define pot A0
+#define relay 12
+#define interruptPin 2
+#define EN 8
 
 // Motor steps per revolution. Most steppers are 200 steps or 1.8 degrees/step
-#define MOTOR_STEPS 200
+#define MOTOR_STEPS 1600
 
 // Since microstepping is set externally, make sure this matches the selected mode
 // If it doesn't, the motor will move at a different RPM than chosen
@@ -41,7 +43,11 @@ LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars
 //BasicStepperDriver stepper(MOTOR_STEPS, DIR, STEP, SLEEP);
 int val, RPM=300;
 float perc;
-int steps = 0, delayTime = 0;
+
+struct config_t
+{
+    int steps, delayTime;
+} configuration;
 
 void setup() {
     lcd.begin();                      // initialize the lcd 
@@ -51,8 +57,13 @@ void setup() {
     stepper.begin(RPM, MICROSTEPS);
     // if using enable/disable on ENABLE pin (active LOW) instead of SLEEP uncomment next line
     // stepper.setEnableActiveState(LOW);
+    EEPROM_readAnything(0, configuration);
     pinMode(pot, INPUT);
     pinMode(relay, OUTPUT);
+    pinMode(EN, OUTPUT);
+    digitalWrite(EN, LOW);
+    pinMode(interruptPin, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(interruptPin), getVelocity, CHANGE);
     digitalWrite(relay, LOW);
     Serial.begin(9600);
 
@@ -68,40 +79,39 @@ void setup() {
     
     Serial.println("Escriba P para pasos, T para tiempo, R para reiniciar");
     Serial.println(" ejemplo P150 para configurar 150 pasos y T1000 para configurar 1 segundo de tiempo");
-    steps = EEPROM.read(0);
-    delayTime = EEPROM.read(1);
 }
 
 void loop() {
      if (Serial.available()){
         serialData = Serial.read();
         if ( serialData == 'P'){
-          steps = Serial.readString().toInt();
-          EEPROM.write(0, steps);
-          Serial.println("pasos: "+ String(steps) );
+          configuration.steps = Serial.readString().toInt();
+          EEPROM_writeAnything(0, configuration);
+          Serial.println("pasos: "+ String(configuration.steps) );
         }
         else if ( serialData == 'T'){
-          delayTime = Serial.readString().toInt();
-          EEPROM.write(1, delayTime);
-          Serial.println("Tiempo (mS): "+ String(delayTime) );
+          configuration.delayTime = Serial.readString().toInt();
+          EEPROM_writeAnything(0, configuration);
+          Serial.println("Tiempo (mS): "+ String(configuration.delayTime) );
         }
         else if (serialData == 'R'){
           Serial.println("Reiniciado con exito!");
           digitalWrite(relay, LOW);
-          delayTime = 0; steps = 0;
+          configuration.delayTime = 0; configuration.steps = 0;
+          EEPROM_writeAnything(0, configuration);
         }
     }
 
     lcd.setCursor(0,0);
-    lcd.print("P:"); lcd.print(steps); lcd.print(" Pot:"); lcd.print(perc); lcd.print("%");
+    lcd.print("P:"); lcd.print(configuration.steps); lcd.print(" Pot:"); lcd.print(perc); lcd.print("%     ");
     lcd.setCursor(0,1);
-    lcd.print("Tiempo:"); lcd.print(delayTime);
+    lcd.print("Tiempo:"); lcd.print(configuration.delayTime); lcd.print("    ");
     
-    if(delayTime > 0 & steps>0)
+    if(configuration.delayTime > 0 & configuration.steps>0)
     {
        Serial.print("Girando.");
        int pasos = 0;
-       while(pasos <= steps)
+       while(pasos <= configuration.steps)
        {
           stepper.move(1); //Paso 1, el motor se mueve
           getVelocity(); // Chequeo si la velocidad ha cambiado
@@ -109,7 +119,7 @@ void loop() {
           pasos++;
        }
        Serial.println(); // Salto de linea
-       delay(delayTime); // Paso 2, el motor espera
+       delay(configuration.delayTime); // Paso 2, el motor espera
        digitalWrite(relay, HIGH); // Paso 3, genera una salida
        Serial.println("Tarea completada con exito!");
        delay(500); // Paso 3, espera mientras sube el piston
@@ -128,4 +138,22 @@ void getVelocity()
     //Serial.print("Potenciometro: "); Serial.print(perc); Serial.println(" %");
     //Serial.print("RPM: "); Serial.println(RPM);
     stepper.setRPM(RPM);
+}
+
+template <class T> int EEPROM_writeAnything(int ee, const T& value)
+{
+    const byte* p = (const byte*)(const void*)&value;
+    unsigned int i;
+    for (i = 0; i < sizeof(value); i++)
+          EEPROM.write(ee++, *p++);
+    return i;
+}
+
+template <class T> int EEPROM_readAnything(int ee, T& value)
+{
+    byte* p = (byte*)(void*)&value;
+    unsigned int i;
+    for (i = 0; i < sizeof(value); i++)
+          *p++ = EEPROM.read(ee++);
+    return i;
 }
